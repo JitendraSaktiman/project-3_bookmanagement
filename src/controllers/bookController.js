573ff917c1265abb6ed0db47b673748374d46d11
Duttji1996 <prakashdutttripathi@gmail.com>
2022-05-16 12:49:55 +0530

    new changes

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -217,7 +217,7 @@ const GetBook = async function (req, res) {
 
         //**********************  If query have no combination of userid,category,subcategory ********************** //
         if(query.title || query.excerpt || query.releasedAt || query.reviews || query._id){
-            return res.status(404).send({ Status: false, message: " You can get data with given filter" }) 
+            return res.status(404).send({ Status: false, message: " You can't get data with given filter" }) 
         }
         
             let FindAllBook = await BookModel.find({ isDeleted: false }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).collation({locale:"en"}).sort({ title: 1 })
